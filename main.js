c68b08f9a4e2f8e220ba529eb938cbb99c5f36be
Unknown <120033812+JstUnknown@users.noreply.github.com>
2026-02-06 17:03:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const imagens=[
+    "assets/foto1.jpeg",
+    "assets/foto2.jpg",
+    "assets/foto3.jpg",
+    "assets/foto4.jpg",
+    "assets/foto5.jpg"
+]
+let indice=0;
+
+//dataset
+const sentimentos={
+    confuso:0,
+    triste:0,
+    feliz:0,
+}
+const imgElement=document.getElementById("img");
+const resultado=document.getElementById("result");
+ 
+
+imgElement.src=imagens[indice];
+function registrar(sentimento){
+    sentimentos[sentimento]++;
+    indice++;
+    if(indice<imagens.length){
+        imgElement.src=imagens[indice]
+    }
+    else{
+        finalizar()
+    }
+}
